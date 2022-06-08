c0978c48b407e072e135e42d55ce07da81117102
leuzaoo <leuzaoocontato@gmail.com>
2022-06-08 14:16:17 -0300

    insert input component and change name files

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState } from "react";
 import "./App.css";
+import AddTask from "./components/addTask";
 
 import Tasks from "./components/Tasks";
 
@@ -20,7 +21,8 @@ const App = () => {
   return (
     <>
       <div className="container">
-        <Tasks tasks={tasks}/>
+        <AddTask />
+        <Tasks tasks={tasks} />
       </div>
     </>
   );
